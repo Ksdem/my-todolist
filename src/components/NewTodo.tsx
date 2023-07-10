938c8e668f887982a9cbea02e0cfa6b8f0e45cbc
ksdem <ksdem9488@gmail.com>
2023-07-10 18:06:13 +0300

    step_05_test

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -2,12 +2,18 @@ import React, {useEffect, useRef, useState} from 'react';
 
 
 function NewTodo() {
-const [block, setBlock]=useState("a")
+const [block, setBlock]=useState(["a","b"])
 const [text, setText]=useState("")
     return <div className="form">
         <input value={text} onChange={event => setText(event.target.value)} />
+
         <div className="button">
-            <button >add</button>
+            <button onClick={()=>{
+                setBlock([...block,text])
+            }}>add</button>
+            <div>
+                {block.map((i=>(<div>{i}</div>)))}
+            </div>
         </div>
     </div>
 }
